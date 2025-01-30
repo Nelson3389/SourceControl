35fdd65994133aa5e368d2704dfc0520ec3312db
Nelson Cruz <nelson.443@icloud.com>
2025-01-30 12:32:45 -0500

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Nelson Cruz on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
