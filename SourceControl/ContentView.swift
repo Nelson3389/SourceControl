1d5e2b64d804e48cbcf098dc9247038e23550235
Nelson Cruz <nelson.443@icloud.com>
2025-01-31 10:31:34 -0500

    Adding "Click me!" button.

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundStyle(.tint)
             Text("I <3 iOS development!")
+            
+            Button("Click me!") {
+                
+            }
         }
         .padding()
         .padding()
