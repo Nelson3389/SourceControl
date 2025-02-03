954018bf0e4ff9a853d24d3bb122d42d0aeed55b
Nelson Cruz <nelson.443@icloud.com>
2025-02-03 10:06:46 -0500

    [Feature] Update Subscribe text to Subscribe now!

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("iOS Development")
             
-            Button("Subscribe!") {
+            Button("Subscribe now!") {
                 
             }
             Rectangle()
