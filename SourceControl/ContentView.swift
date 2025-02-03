803dd8eeeb9f009f5332ca19c4b9a5d3005e075b
Nelson Cruz <nelson.443@icloud.com>
2025-02-03 11:51:24 -0500

    [Clean] Updates to documentation.

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -5,6 +5,34 @@
 //  Created by Nelson Cruz on 1/30/25.
 //
 
+/*
+Clone = Copying the repo locally
+Commit = Save ("checkpoint") on our current branch
+Stage = Prepare changes for a commit
+Stash = Save changes for later
+Push = Send local commits to remote repo
+Pull = Fetch remote commits to local repo
+
+COMMIT MESSAGES
+
+NEW FEATURE:
+[Feature] Description of the feature
+
+BUG NOT IN PRODUCTION:
+[Bug] Description of the bug
+
+RELEASE:
+[Release] Description of release
+
+BUG IN PRODUCTION:
+[Patch] Description of patch
+
+MUNDANE TASKS:
+[Clean] Description of changes
+ 
+*/
+
+
 import SwiftUI
 
 struct ContentView: View {
