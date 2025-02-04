4e4e6ee7046454814f75dfe71a62386e8f4bdb95
Nelson Cruz <nelson.443@icloud.com>
2025-02-04 09:02:26 -0500

    [Feature] Add Explore View.

---
+++ SourceControl/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  SourceControl
+//
+//  Created by Nelson Cruz on 2/4/25.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExploreView()
+}
